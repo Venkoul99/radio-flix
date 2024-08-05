eb1a6c9325c99d60cb88119db77173eb6d5df2b6
Venelin <venkoul99@gmail.com>
2024-08-05 21:47:45 +0300

    Move NewsItemDetails out of News component

--- client/src/pages/news/NewsItemDetails.tsx
+++ client/src/pages/news/NewsItemDetails.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header/Header';
 import { Footer } from '@/components/Footer/Footer';
-import NewsItemDetails from '@/components/News/NewsItemDetails/NewsItemDetails';
+import NewsItemDetails from '@/components/NewsItemDetails/NewsItemDetails';
 
 export function NewsItemDetailsPage() {
   return (
